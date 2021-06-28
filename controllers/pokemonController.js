3331cce99f521a4575367fcb72d1fa3cb68d7cfc
jayhar006 <jayharvallejos12@gmail.com>
2021-06-28 16:59:03 +0800

    first commit

---
+++ controllers/pokemonController.js
@@ -0,0 +1,135 @@
+const { pokemonService } = require('../services');
+const url = require('url');
+const { update } = require('../services/pokemonService');
+
+exports.handleGetRequest = (req, res) => {
+    const pokemonUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    let data;
+    if (pokemonUrl.toString().includes('?name')) {
+        console.log(pokemonUrl.searchParams);
+        const params = pokemonUrl.searchParams;
+        data = pokemonService.getByName(params.get('name'));
+    } else {
+        data = pokemonService.get();
+    }
+
+    const result = { data };
+
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+    });
+    res.write(JSON.stringify(result));
+    res.end();
+};
+
+exports.handlePostRequest = (req, res) => {
+    const data = [];
+
+    req.on('data', (chunk) => {
+        data.push(chunk);
+    });
+
+    req.on('end', () => {
+        const parsedData = Buffer.concat(data).toString();
+        const dataJson = JSON.parse(parsedData);
+
+        const result = pokemonService.insert(dataJson);
+
+        if (!result.success) {
+            res.writeHead(400, {
+                'Content-Type': 'application/json',
+            });
+            res.write(JSON.stringify(result));
+            res.end();
+        }
+
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+        });
+        res.write(JSON.stringify(result));
+        res.end();
+    });
+};
+
+exports.handlePutRequest = (req, res) => {
+    const pokemonUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    if (pokemonUrl.toString().includes('?name')) {
+        const params = pokemonUrl.searchParams;
+        const pokeName = params.get('name');
+
+        const data = [];
+
+        req.on('data', (chunk) => {
+            data.push(chunk);
+        });
+
+        req.on('end', () => {
+
+            const parsedData = Buffer.concat(data).toString();
+            const dataJson = JSON.parse(parsedData);
+
+            const result = pokemonService.update(pokeName, dataJson);
+
+            if (!result.success) {
+                res.writeHead(400, {
+                    'Content-Type': 'application/json',
+                });
+                res.write(JSON.stringify(result));
+                res.end();
+            } else {
+    
+                res.writeHead(200, {
+                    'Content-Type': 'application/json',
+                });
+                res.write(JSON.stringify(result));
+                res.end();
+            }
+
+        });
+
+    } else {
+        const errorMsg = {
+            success: false,
+            errorMessage: `name parameter is missing.`,
+        };
+
+        res.writeHead(400, {
+            'Content-Type': 'application/json',
+        });
+        res.write(JSON.stringify(errorMsg));
+        res.end();
+
+    }
+
+};
+
+exports.handleDeleteRequest = (req, res) => {
+    const pokemonUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    if (pokemonUrl.toString().includes('?name')) {
+
+        const params = pokemonUrl.searchParams;
+        const result = pokemonService.delete(params.get('name'));
+
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+        });
+        res.write(JSON.stringify(result));
+        res.end();
+
+    } else {
+        const errorMsg = {
+            success: false,
+            errorMessage: `name parameter is missing.`,
+        };
+
+        res.writeHead(400, {
+            'Content-Type': 'application/json',
+        });
+        res.write(JSON.stringify(errorMsg));
+        res.end();
+
+    }
+};
---
+++ services/pokemonService.js
@@ -0,0 +1,119 @@
+const lowdb = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('../pokemon.json');
+const db = lowdb(adapter);
+
+db.defaults({ pokemons: [] }).write();
+
+exports.get = () => {
+    const pokemons = db.get('pokemons').value();
+
+    return pokemons;
+};
+
+exports.insert = (pokemon) => {
+    const { name } = pokemon;
+
+    const isPokemonExist =
+        db
+            .get('pokemons')
+            .value()
+            .filter((_) => _.name === name).length > 0;
+
+    if (isPokemonExist) {
+        return {
+            success: false,
+            errorMessage: `Pokemon ${name} already exist.`,
+        };
+    }
+
+    db.get('pokemons').push(pokemon).write();
+
+    return {
+        success: true,
+    };
+};
+
+exports.get = () => {
+    const pokemons = db.get(pokemonEndpoint).value();
+
+    return pokemons;
+};
+
+exports.getByName = (pokeName) => {
+    const pokemon = db.get(pokemonEndpoint)
+                        .filter(n => compareName(n, pokeName));
+
+    return pokemon;
+};
+
+exports.delete = (pokeName) => {
+    const isPokemonPresent = db.get(pokemonEndpoint)
+     .value()
+     .filter(n => compareName(n, pokeName));
+ 
+     if (isPokemonPresent.length === 0) {
+         return {
+             success: false,
+             errorMessage: `Pokemon ${pokeName} not found.`,
+         };
+     }
+ 
+     db.get(pokemonEndpoint)
+     .remove(n => compareName(n, pokeName))
+     .write();
+ 
+     return {
+         success: true,
+     };
+ 
+ };
+
+exports.update = (pokeName, dataJson) => {
+    const isPokemonPresent = db.get(pokemonEndpoint)
+    .value()
+    .filter(n => compareName(n, pokeName));
+
+    if (isPokemonPresent.length === 0) {
+        return {
+            success: false,
+            errorMessage: `Pokemon ${pokeName} not found.`,
+        };
+    }
+
+    if (dataJson['type'] === "") {
+        return {
+            success: false,
+            errorMessage: `type property is blank`,
+        };
+    }
+
+    if (!JSON.stringify(dataJson).includes('type')) {
+        return {
+            success: false,
+            errorMessage: `type property is missing`,
+        };
+    }
+
+    db.get(pokemonEndpoint)
+        .find({ name: pokeName })
+        .assign(dataJson)
+        .write();
+
+    return {
+        success: true,
+    };
+
+};
+
+
+
+
+const compareName = (n, pokeName) => {
+    if (n.name.localeCompare(pokeName, undefined, { sensitivity: 'accent' }) === 0) {
+        return true;
+    }
+    return false;
+};
+
+
